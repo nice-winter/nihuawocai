75de1d09cd1cd515e75eb781bc4ebbc15f5e75cc
nice-winter <doyouknowmeemail@gmail.com>
2025-10-23 15:43:25 +0800

    chore(components): rename

--- app/app.vue
+++ app/app.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import NButton from './components/ui/NButton.vue'
-
 const { data } = await useFetch('/api/hello')
 console.log(data.value)
 </script>
 
 <template>
   <UApp>
-    <NButton size="large" color="red" @click="() => console.log('快速开始')">快速开始</NButton>
-    <NButton size="small">加入</NButton>
-    <NButton>创建房间</NButton>
-    <NButton>等待房间</NButton>
-    <NButton color="green">发送广播</NButton>
-    <NButton color="blue">邀请好友</NButton>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UiButton size="large" color="red" @click="() => console.log('快速开始')">快速开始</UiButton>
+    <UiButton size="small">加入</UiButton>
+    <UiButton>创建房间</UiButton>
+    <UiButton>等待房间</UiButton>
+    <UiButton color="green">发送广播</UiButton>
+    <UiButton color="blue">邀请好友</UiButton>
+
+    <UiGameMainFrame />
+
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
   </UApp>
 </template>
