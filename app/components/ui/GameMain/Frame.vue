df4b02f09e6bf78650145e4ade1a0cc32395c197
nice-winter <doyouknowmeemail@gmail.com>
2025-10-23 16:29:33 +0800

    chore(components): update game ui

--- app/components/ui/GameMain/Frame.vue
+++ app/components/ui/GameMain/Frame.vue
@@ -1,40 +1,51 @@
 <template>
-  <div class="n-game-main-frame">
-    <div class="n-game-main-frame__panel panel-foreground"></div>
-    <div class="n-game-main-frame__panel panel-background"></div>
+  <div class="game-main-frame">
+    <div class="game-main-frame__header">
+      <div class="game-main-frame__header slot-wrapper">
+        <slot name="header" />
+      </div>
+    </div>
+
+    <div class="game-main-frame__panel">
+      <slot name="panel" />
+    </div>
+
+    <div class="game-main-frame__footer">
+      <div class="game-main-frame__footer slot-wrapper">
+        <slot name="footer" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="less" scoped>
-.n-game-main-frame {
+.game-main-frame {
   position: relative;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
 }
 
-.n-game-main-frame__panel {
-  width: 900px;
-  height: 640px;
-  border-radius: 14px;
-  box-shadow: 0 0 4px 2px rgb(176 129 73);
+.game-main-frame {
+  & > &__header,
+  & > &__footer {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  &.panel-foreground {
-    z-index: 2;
-    border: 1px solid #eee;
-    background-color: #f7ebdf;
-    background-image:
-      linear-gradient(#eae0d0 1px, transparent 1px),
-      linear-gradient(90deg, #eae0d0 1px, transparent 1px);
-    background-size: 16px 16px;
+    div.slot-wrapper {
+      width: 900px;
+      height: 100%;
+    }
   }
-  &.panel-background {
-    z-index: 1;
-    position: absolute;
-    transform: rotate(-1.6deg);
-    background-color: #cdb69b;
+
+  & > &__header {
+    height: 3rem;
+  }
+  & > &__footer {
+    height: 5rem;
   }
 }
 </style>
---
+++ app/components/ui/GameMain/Panel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="game-main-panel">
+    <div class="game-main-panel__panel foreground">
+      <slot />
+    </div>
+    <div class="game-main-panel__panel background" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.game-main-panel {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.game-main-panel__panel {
+  width: 900px;
+  height: 640px;
+  border-radius: 14px;
+  box-shadow: 0 0 4px 2px rgb(176 129 73);
+
+  &.foreground {
+    z-index: 2;
+    border: 1px solid #eee;
+    background-color: #f7ebdf;
+    background-image:
+      linear-gradient(#eae0d0 1px, transparent 1px),
+      linear-gradient(90deg, #eae0d0 1px, transparent 1px);
+    background-size: 16px 16px;
+  }
+  &.background {
+    z-index: 1;
+    position: absolute;
+    transform: rotate(-1.6deg);
+    background-color: #cdb69b;
+  }
+}
+</style>
