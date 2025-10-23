b83f41c4b6c4678039c2ec29616ceb96d8f36ba6
nice-winter <doyouknowmeemail@gmail.com>
2025-10-23 16:29:33 +0800

    chore(components): update game ui

--- app/app.vue
+++ app/app.vue
@@ -5,14 +5,20 @@ console.log(data.value)
 
 <template>
   <UApp>
-    <UiButton size="large" color="red" @click="() => console.log('快速开始')">快速开始</UiButton>
-    <UiButton size="small">加入</UiButton>
-    <UiButton>创建房间</UiButton>
-    <UiButton>等待房间</UiButton>
-    <UiButton color="green">发送广播</UiButton>
-    <UiButton color="blue">邀请好友</UiButton>
-
-    <UiGameMainFrame />
+    <UiGameMainFrame>
+      <template #panel>
+        <UiGameMainPanel>
+          <UiButton size="large" color="red" @click="() => console.log('快速开始')">
+            快速开始
+          </UiButton>
+          <UiButton size="small">加入</UiButton>
+          <UiButton>创建房间</UiButton>
+          <UiButton>等待房间</UiButton>
+          <UiButton color="green">发送广播</UiButton>
+          <UiButton color="blue">邀请好友</UiButton>
+        </UiGameMainPanel>
+      </template>
+    </UiGameMainFrame>
 
     <!-- <NuxtRouteAnnouncer />
     <NuxtWelcome /> -->
