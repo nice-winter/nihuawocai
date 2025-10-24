2a6ab4a2db10cd83d9ed9ca96bd6f5b6d9addc2b
nice-winter <doyouknowmeemail@gmail.com>
2025-10-25 01:01:23 +0800

    perf: move to tailwindcss

--- app/components/ui/GameMain/Frame.vue
+++ app/components/ui/GameMain/Frame.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="game-main-frame">
-    <div class="game-main-frame__header">
-      <div class="game-main-frame__header slot-wrapper">
+  <div class="relative flex flex-col">
+    <div class="relative flex justify-center items-center h-12">
+      <div class="w-[900px] h-full">
         <slot name="header" />
       </div>
     </div>
 
-    <div class="game-main-frame__panel">
+    <div>
       <slot name="panel" />
     </div>
 
-    <div class="game-main-frame__footer">
-      <div class="game-main-frame__footer slot-wrapper">
+    <div class="relative flex justify-center items-center h-20">
+      <div class="w-[900px] h-full">
         <slot name="footer" />
       </div>
     </div>
@@ -19,33 +19,3 @@
 </template>
 
 <script setup lang="ts"></script>
-
-<style lang="less" scoped>
-.game-main-frame {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-}
-
-.game-main-frame {
-  & > &__header,
-  & > &__footer {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    div.slot-wrapper {
-      width: 900px;
-      height: 100%;
-    }
-  }
-
-  & > &__header {
-    height: 3rem;
-  }
-  & > &__footer {
-    height: 5rem;
-  }
-}
-</style>
