889eede70dca6279239cc3e761752033ba01ca27
nice-winter <doyouknowmeemail@gmail.com>
2025-10-25 01:43:51 +0800

    chore: nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  // debug: true,
   ssr: false,
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
