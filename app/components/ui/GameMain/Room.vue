0886b1c270518a4f5a65c9f2234031ebfbdd445c
nice-winter <doyouknowmeemail@gmail.com>
2025-10-25 03:48:51 +0800

    perf: style

--- app/components/ui/GameMain/Room.vue
+++ app/components/ui/GameMain/Room.vue
@@ -1,34 +1,21 @@
 <template>
-  <div class="game-main-panel__room">
-    <div class="room left-wrapper">
-      <div class="room-header">
-        <div class="base-info">
+  <div class="flex flex-row h-full">
+    <div class="flex flex-col w-[74.4%]">
+      <div
+        class="px-[0.785rem] h-[21.05%] border-b-4 border-[#eeddcb] flex flex-row gap-[0.785rem] relative"
+      >
+        <div class="flex flex-col justify-center gap-[0.785rem] w-36">
           <ui-game-main-room-number :room-number="roomInfo?.roomNumber || 0" />
-          <span
-            style="
-              padding-left: 4px;
-              position: relative;
-              display: inline-flex;
-              vertical-align: text-bottom;
-              align-items: center;
-              gap: 4px;
-            "
-          >
-            <UCheckbox
-              v-model="locked"
-              size="sm"
-              icon="ph:check-bold"
-              style="border-radius: 0; background-color: #f7ede3"
-            />
+          <span class="relative inline-flex items-center gap-1 pl-1 align-text-bottom">
+            <UCheckbox v-model="locked" size="sm" icon="ph:check-bold" class="game-checkbox" />
             <span
-              style="cursor: pointer; font-size: 14px"
-              :style="{ minWidth: !password ? `inherit` : `5rem` }"
-              class="select-none"
+              class="cursor-pointer text-sm select-none"
+              :class="{ 'min-w-20': password, 'min-w-inherit': !password }"
               @click="changePassword"
             >
               {{ pwdText }}
             </span>
-            <span v-show="showPasswordInput" style="position: absolute; left: 58px; width: 4rem">
+            <span v-show="showPasswordInput" class="absolute left-[58px] w-16">
               <UInput
                 ref="passwordInput"
                 class="game-input"
@@ -43,14 +30,14 @@
           </span>
         </div>
 
-        <div class="room-log">
-          <div class="text-wrapper">
+        <div class="flex flex-col justify-center">
+          <div class="w-48 h-20 text-[13px] text-[#c3b4a0]">
             <UiGameMainRoomMessageList ref="RoomMessageList" />
           </div>
         </div>
 
-        <div class="actions">
-          <div class="button-wrapper">
+        <div class="flex flex-col justify-center ml-4">
+          <div class="grid grid-cols-2 grid-rows-2 gap-[0.785rem]">
             <UiButton color="green">广播邀请</UiButton>
             <UiButton color="blue">邀请好友</UiButton>
             <UiButton>再等一会</UiButton>
@@ -58,24 +45,29 @@
           </div>
         </div>
       </div>
-      <div class="room-main">
-        <div v-for="i in 8" :key="i" class="player-seat select-none" :class="[`seat-${i}`]">
-          <div class="seat-box">
+      <div class="grow grid grid-rows-2 grid-cols-4 p-8 gap-[0.785rem] justify-center">
+        <div
+          v-for="i in 8"
+          :key="i"
+          class="flex flex-col items-center justify-center gap-[0.785rem] select-none"
+          :class="[`seat-${i}`]"
+        >
+          <div class="flex items-center justify-center w-[114px] h-[114px] bg-[#ddc9a9] text-sm">
             <span>等待玩家</span>
           </div>
 
-          <p class="player-seat__nickname">玩家昵称ABC哈哈哈</p>
+          <p class="text-sm text-center">玩家昵称ABC哈哈哈</p>
         </div>
       </div>
     </div>
-    <div class="room right-wrapper">
-      <div class="invitation-tab-wrapper">
+    <div class="grow flex flex-col bg-[#f1d0ae42] border-l-2 border-white/60">
+      <div class="mt-16 p-0">
         <UTabs :items="tabItems" size="xs" class="game-tabs select-none">
           <template #user1>
-            <div class="invitation-tab__players"></div>
+            <div class="h-56"></div>
           </template>
           <template #user2>
-            <div class="invitation-tab__players"></div>
+            <div class="h-56"></div>
           </template>
         </UTabs>
       </div>
@@ -168,111 +160,4 @@ const tabItems = [
 ]
 </script>
 
-<style scoped>
-.game-main-panel__room {
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-}
-
-.room.left-wrapper {
-  display: flex;
-  flex-direction: column;
-  width: 74.4%;
-
-  .room-header {
-    padding: 0 0.785rem;
-    height: 21.05%;
-    border-bottom: 4px solid #eeddcb;
-    display: flex;
-    flex-direction: row;
-    gap: 0.785rem;
-    position: relative;
-
-    .base-info {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      gap: 0.785rem;
-      width: 144px;
-    }
-
-    .room-log {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-
-      .text-wrapper {
-        width: 12rem;
-        height: 5rem;
-        font-size: 13px;
-        color: #c3b4a0;
-      }
-    }
-
-    .actions {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      margin-left: 1rem;
-
-      .button-wrapper {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-template-rows: 1fr 1fr;
-        gap: 0.785rem;
-      }
-    }
-  }
-  .room-main {
-    flex-grow: 1;
-    display: grid;
-    grid-template-rows: 1fr 1fr;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    padding: 2rem;
-    gap: 0.785rem;
-
-    justify-content: center;
-
-    .player-seat {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      gap: 0.785rem;
-
-      .seat-box {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 114px;
-        height: 114px;
-        background-color: #ddc9a9;
-        font-size: 14px;
-      }
-
-      p.player-seat__nickname {
-        font-size: 14px;
-        text-align: center;
-      }
-    }
-  }
-}
-
-.room.right-wrapper {
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  background-color: #f1d0ae42;
-  border-left: 2px solid color(srgb 1 1 1 / 0.6);
-
-  .invitation-tab-wrapper {
-    margin-top: 4rem;
-    padding: 0;
-  }
-
-  .invitation-tab__players {
-    height: 14rem;
-  }
-}
-</style>
+<style scoped></style>
