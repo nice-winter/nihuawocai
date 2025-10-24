06fab6578be05313848dca3cdeb28713b43d48f1
nice-winter <doyouknowmeemail@gmail.com>
2025-10-24 11:32:56 +0800

    feat(components/RoomNumber): add RoomNumber

---
+++ app/components/ui/GameMain/RoomNumber.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="room-number">{{ displayText }}</span>
+</template>
+
+<script setup lang="ts">
+interface RoomNumberProps {
+  roomNumber: number
+}
+
+const { roomNumber } = defineProps<RoomNumberProps>()
+
+const displayText = computed(() => roomNumber.toString().padStart(3, '0'))
+</script>
+
+<style scoped>
+span.room-number {
+  font-size: 3.5rem;
+  line-height: 3rem;
+  font-weight: bolder;
+  font-style: italic;
+  color: #ecd3c1;
+  text-shadow: -2px -0.5px 0px #cbad91;
+  user-select: none;
+}
+</style>
