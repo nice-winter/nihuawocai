1a5ffb3fc4902aef022c81575ea67059373916b2
nice-winter <doyouknowmeemail@gmail.com>
2025-10-25 00:50:43 +0800

    perf: move to tailwindcss

--- app/components/ui/GameMain/Panel.vue
+++ app/components/ui/GameMain/Panel.vue
@@ -1,50 +1,33 @@
 <template>
-  <div class="game-main-panel">
-    <div class="game-main-panel__panel foreground">
+  <div class="relative flex justify-center items-center">
+    <div
+      class="relative z-20 w-[900px] h-[640px] rounded-[14px] overflow-hidden border border-[#eae0d0] bg-[#f7ebdf] shadow-[0_0_4px_2px_rgb(176,129,73)] custom-foreground-bg"
+    >
       <slot />
     </div>
-    <div class="game-main-panel__panel background" />
+    <div
+      class="absolute z-10 w-[900px] h-[640px] rounded-[14px] bg-[#cdb69b] shadow-[0_0_4px_2px_rgb(176,129,73)] rotate-[-1.6deg] custom-background-bg"
+    />
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="less" scoped>
-.game-main-panel {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<style scoped>
+.custom-foreground-bg {
+  background-image:
+    linear-gradient(#eae0d0 1px, transparent 1px),
+    linear-gradient(90deg, #eae0d0 1px, transparent 1px);
+  background-size: 16px 16px;
 }
 
-.game-main-panel__panel {
-  width: 900px;
-  height: 640px;
-  border-radius: 14px;
-  box-shadow: 0 0 4px 2px rgb(176 129 73);
-  overflow: hidden;
-
-  &.foreground {
-    z-index: 2;
-    border: 1px solid #eae0d0;
-    background-color: #f7ebdf;
-    background-image:
-      linear-gradient(#eae0d0 1px, transparent 1px),
-      linear-gradient(90deg, #eae0d0 1px, transparent 1px);
-    background-size: 16px 16px;
-  }
-  &.background {
-    z-index: 1;
-    position: absolute;
-    transform: rotate(-1.6deg);
-    background-color: #cdb69b;
-    background-image: repeating-linear-gradient(
-      -45deg,
-      #b19b79 0,
-      #c3b6a2 1px,
-      #cdb59a 1px,
-      #cdb59a 3px
-    );
-  }
+.custom-background-bg {
+  background-image: repeating-linear-gradient(
+    -45deg,
+    #b19b79 0,
+    #c3b6a2 1px,
+    #cdb59a 1px,
+    #cdb59a 3px
+  );
 }
 </style>
