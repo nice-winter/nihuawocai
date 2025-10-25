26bd5f56acca0a151ef7e295f1cfbf096ae3b597
nice-winter <doyouknowmeemail@gmail.com>
2025-10-26 07:37:52 +0800

    feat: new app layout

--- app/app.vue
+++ app/app.vue
@@ -5,13 +5,46 @@ console.log(data.value)
 
 <template>
   <UApp>
-    <UiGameMainFrame>
-      <template #panel>
-        <UiGameMainPanel>
-          <!-- <UiGameMainLobby /> -->
-          <UiGameMainRoom />
-        </UiGameMainPanel>
-      </template>
-    </UiGameMainFrame>
+    <div class="w-full flex flex-row justify-center">
+      <div class="w-5xl min-h-[1100px] flex flex-col custom-bg bg-top bg-size-[1024px]">
+        <div class="w-full h-40"></div>
+        <div class="w-full h-10 flex justify-center items-center">
+          <div class="w-[900px] flex flex-row justify-center">
+            <span class="font-sans text-5xl quotation" style="margin-left: -28px">“</span>
+            <span class="grow text-light text-[#432410] px-2">
+              【公告】阿星啊，你总得计划一下吧，这么年轻难道不工作，就混小太保啊。
+            </span>
+            <span class="font-sans text-5xl quotation" style="margin-right: -28px">”</span>
+          </div>
+        </div>
+        <UiGameMainFrame class="">
+          <template #panel>
+            <UiGameMainPanel>
+              <UiGameMainLobby />
+              <!-- <UiGameMainRoom /> -->
+            </UiGameMainPanel>
+          </template>
+        </UiGameMainFrame>
+      </div>
+    </div>
   </UApp>
 </template>
+
+<style scoped>
+.custom-bg {
+  background-image: url(~/assets/bg.jpg);
+  background-repeat: no-repeat;
+}
+
+.quotation {
+  line-height: 0;
+  font-family: sans-serif;
+  text-shadow:
+    2px 2px 0 #ffffff,
+    -1px 1px 0 #ffffff,
+    2px 1px 0 #ffffff;
+  color: #db0030;
+  padding-top: 20px;
+  user-select: none;
+}
+</style>
