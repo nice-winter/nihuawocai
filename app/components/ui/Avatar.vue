1a8bfee09d4f7864e7772d71127e811f308a1f6c
nice-winter <doyouknowmeemail@gmail.com>
2025-10-25 18:38:17 +0800

    perf: style

---
+++ app/components/ui/Avatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <UPopover
+    :content="{
+      align: 'end',
+      side: 'right',
+      sideOffset: 8
+    }"
+    arrow
+  >
+    <UAvatar class="game-avatar" :src="player.avatar" />
+
+    <template #content>
+      <div class="w-30 h-50 inline-flex bg-[#f5e4d2]">
+        <UAvatar class="game-avatar w-30 h-30" :src="player.avatar" />
+      </div>
+    </template>
+  </UPopover>
+</template>
+
+<script setup lang="ts">
+import type { Player } from '~/interfaces/player'
+
+interface AvatarProps {
+  player: Player
+}
+
+const { player } = defineProps<AvatarProps>()
+</script>
+
+<style scoped></style>
