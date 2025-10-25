5c718eeec159a9ddee83fed9a11aa66ed7d7e6c3
nice-winter <doyouknowmeemail@gmail.com>
2025-10-26 01:37:09 +0800

    feat(Avatar): add seat mode

--- app/components/ui/Avatar.vue
+++ app/components/ui/Avatar.vue
@@ -1,30 +1,65 @@
 <template>
-  <UPopover
-    :content="{
-      align: 'end',
-      side: 'right',
-      sideOffset: 8
-    }"
-    arrow
+  <span
+    class="inline-flex items-center justify-center size-8 bg-[#ddc9a9]"
+    :class="{ 'cursor-pointer': !disabled }"
+    @click="onClick"
   >
-    <UAvatar class="game-avatar" :src="player.avatar" />
+    <UPopover
+      :content="{
+        align: 'end',
+        side: 'right',
+        sideOffset: 8
+      }"
+      arrow
+    >
+      <UAvatar v-if="player" class="game-avatar size-full cursor-pointer" :src="player.avatar" />
+      <template #content>
+        <div class="w-30 h-50 inline-flex bg-[#f5e4d2]">
+          <UAvatar class="game-avatar w-30 h-30" :src="player?.avatar" />
+        </div>
+      </template>
+    </UPopover>
 
-    <template #content>
-      <div class="w-30 h-50 inline-flex bg-[#f5e4d2]">
-        <UAvatar class="game-avatar w-30 h-30" :src="player.avatar" />
-      </div>
+    <slot v-if="!player || !player.avatar" name="placeholder" />
+
+    <template v-if="mode === 'seat' && !player">
+      <UIcon v-if="!open" :name="`fe:disabled`" class="text-[#E20032] size-14" />
+      <span v-if="open">等待玩家</span>
     </template>
-  </UPopover>
+  </span>
 </template>
 
 <script setup lang="ts">
 import type { Player } from '~/interfaces/player'
 
 interface AvatarProps {
-  player: Player
+  id?: number | string
+  mode?: 'display' | 'seat'
+  disabled?: boolean
+  player?: Player
 }
 
-const { player } = defineProps<AvatarProps>()
+const {
+  id = undefined,
+  mode = 'display',
+  disabled = true,
+  player = undefined
+} = defineProps<AvatarProps>()
+
+const open = defineModel<boolean>({ default: true })
+
+const emit = defineEmits<{
+  (e: 'switch', open: boolean, id?: number | string): void
+}>()
+
+const onClick = () => {
+  if (mode === 'seat' && !disabled) {
+    if (!player) {
+      open.value = !open.value
+      emit('switch', open.value, id)
+    }
+  }
+}
 </script>
 
 <style scoped></style>
