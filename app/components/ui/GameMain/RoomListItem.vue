94f0dabe44cfb67ae0d27aeba9fc4e330ca3e300
nice-winter <doyouknowmeemail@gmail.com>
2025-10-26 01:36:38 +0800

    refactor(RoomListItem): support seats

--- app/components/ui/GameMain/RoomListItem.vue
+++ app/components/ui/GameMain/RoomListItem.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="p-[0.785rem] w-1/2">
     <div class="flex flex-col gap-[0.7rem] select-none">
-      <div class="relative">
-        <UiAvatar v-if="players[0]" class="size-16" :player="players[0]" />
+      <div class="relative h-16">
+        <UiAvatar v-if="author" class="size-16" :player="authorPlayer" />
         <UTooltip text="非公开房间" :delay-duration="500" :content="{ side: 'right' }" arrow>
           <UIcon
             v-show="locked"
@@ -12,18 +12,32 @@
         </UTooltip>
         <UiGameMainRoomNumber :room-number="roomNumber || 0" class="float-right" />
         <span class="absolute right-0 bottom-0 text-sm leading-3.5">
-          <UiGenderIcon :gender="players[0]?.gender" class="align-text-top" />
-          {{ players[0]?.nickname }}
+          <UiGenderIcon :gender="authorPlayer.gender" class="align-text-top" />
+          {{ authorPlayer.nickname }}
         </span>
       </div>
-      <div class="flex justify-between">
-        <template v-for="i in 6" :key="players[i]?.uuid">
-          <UiAvatar v-if="players[i]" class="size-[46px]" :player="players[i]" />
-          <span v-else class="inline-block w-[46px] h-[46px] bg-[#ddc9a9]" />
+
+      <div
+        class="flex min-h-[46px]"
+        :class="[openSeatCount >= 7 ? `justify-between` : `gap-[6.666px]`]"
+      >
+        <template v-for="(i, index) in openSeatCount - 1" :key="playersWithOutAuthor[index]?.uuid">
+          <UiAvatar
+            v-if="playersWithOutAuthor[index]"
+            class="size-[46px]"
+            :player="playersWithOutAuthor[index]"
+          />
+          <span v-else class="inline-block size-[46px] bg-[#ddc9a9]" />
         </template>
       </div>
+
       <div class="pl-[1.5px]">
-        <UiButton v-show="!playing" size="xl" color="red" :disabled="players.length >= 7">
+        <UiButton
+          v-show="!playing"
+          size="xl"
+          color="red"
+          :disabled="totalPlayerCount >= openSeatCount"
+        >
           加ㅤ入
         </UiButton>
 
@@ -36,14 +50,20 @@
 </template>
 
 <script setup lang="ts">
-import type { Player } from '~/interfaces/player'
 import type { RoomListItemProps } from '~/interfaces/room'
 
 const {
   roomNumber = undefined,
+  author = undefined,
   players = [],
   onlookers = [],
   playing = false,
+  seats = [],
   locked = false
 } = defineProps<RoomListItemProps>()
+
+const playersWithOutAuthor = computed(() => players.filter((p) => p !== null && p.uuid !== author))
+const authorPlayer = computed(() => players.find((p) => p?.uuid === author)!)
+const totalPlayerCount = computed(() => players.filter((p) => p).length)
+const openSeatCount = computed(() => seats.filter((s) => s).length)
 </script>
