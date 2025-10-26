774fc1d32488754f21a095f66392c4bbc4051ab2
nice-winter <doyouknowmeemail@gmail.com>
2025-10-27 02:51:00 +0800

    feat(LinkButton): new components

---
+++ app/components/ui/LinkButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <span
+    class="flex flex-cow gap-1 text-(--game-font-color) hover:text-default cursor-pointer select-none"
+  >
+    <ULink as="button" class="text-[13px] text-light cursor-pointer transition-none">
+      <slot />
+    </ULink>
+
+    <UIcon v-if="icon" :name="icon" />
+  </span>
+</template>
+
+<script setup lang="ts">
+const { icon = undefined } = defineProps<{
+  icon?: string
+}>()
+</script>
+
+<style scoped></style>
