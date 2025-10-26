0583037eb233996de7c231fa921f2ad352831ea0
nice-winter <doyouknowmeemail@gmail.com>
2025-10-26 22:21:57 +0800

    perf(app): style

--- app/app.vue
+++ app/app.vue
@@ -6,12 +6,12 @@ console.log(data.value)
 <template>
   <UApp>
     <div class="w-full flex flex-row justify-center">
-      <div class="w-5xl min-h-[1100px] flex flex-col custom-bg bg-top bg-size-[1024px]">
+      <div class="w-5xl min-h-[1151px] flex flex-col custom-bg bg-top bg-size-[1024px]">
         <div class="w-full h-40"></div>
         <div class="w-full h-10 flex justify-center items-center">
           <div class="w-[900px] flex flex-row justify-center">
             <span class="font-sans text-5xl quotation" style="margin-left: -28px">“</span>
-            <span class="grow text-light text-(--game-primary-color) px-2">
+            <span class="grow text-light px-2">
               【公告】阿星啊，你总得计划一下吧，这么年轻难道不工作，就混小太保啊。
             </span>
             <span class="font-sans text-5xl quotation" style="margin-right: -28px">”</span>
