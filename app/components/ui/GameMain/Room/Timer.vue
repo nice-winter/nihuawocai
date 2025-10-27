b87c1a082b4fd71b48788b72b88ee04fb4e0638a
nice-winter <doyouknowmeemail@gmail.com>
2025-10-28 05:05:09 +0800

    feat(Timer): new components

---
+++ app/components/ui/GameMain/Room/Timer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="size-16 flex items-center justify-center">
+    <div class="size-10 flex items-center justify-center rounded-full bg-[#E31839] timer">
+      <span class="text-2xl text-white font-bold leading-0 select-none">13</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.timer {
+  box-shadow:
+    0 0 0 4px #ffffff,
+    0 0 0 6px #e31839;
+}
+</style>
