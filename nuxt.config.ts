f45987192b75f7e3c4640401a3e13a29928da64e
nice-winter <doyouknowmeemail@gmail.com>
2025-10-28 16:05:38 +0800

    chore(nuxt.config.ts): enable typescriptPlugin support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
+  experimental: {
+    typescriptPlugin: true
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
