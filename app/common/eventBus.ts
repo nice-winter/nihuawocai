6e28cfb55aa01a4c3b275ad847298eae566bb37a
nice-winter <doyouknowmeemail@gmail.com>
2025-10-31 04:53:56 +0800

    feat(eventBus): useEventBus

--- app/common/eventBus.ts
+++ app/common/eventBus.ts
@@ -7,3 +7,11 @@ type Events = {
 }
 
 export const eventBus = mitt<Events>()
+
+type EventKeys = keyof Events
+type EventHandler<K extends EventKeys> = (payload: Events[K]) => void
+
+export function useEventBus<K extends EventKeys>(event: K, handler: EventHandler<K>) {
+  onMounted(() => eventBus.on(event, handler))
+  onUnmounted(() => eventBus.off(event, handler))
+}
