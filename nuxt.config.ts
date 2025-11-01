ddd423482098b226150121a7459033d59396c4fb
nice-winter <doyouknowmeemail@gmail.com>
2025-11-01 22:55:39 +0800

    feat(config): 添加 Vite 服务器配置以支持 HMR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  vite: {
+    server: {
+      hmr: {
+        path: 'hmr/'
+      }
+    }
+  },
   ssr: false,
   experimental: {
     typescriptPlugin: true
