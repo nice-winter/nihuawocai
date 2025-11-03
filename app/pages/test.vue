ea9702dc3c448beb30a768590f71fa071625fb60
nice-winter <doyouknowmeemail@gmail.com>
2025-11-04 05:05:43 +0800

    chore: 添加测试用页面

---
+++ app/pages/test.vue
@@ -0,0 +1,14 @@
+<
+<template>
+  <div>
+    <Text :text="text" :emojis="[]" />
+  </div>
+</template>
+
+<script setup>
+import Text from '~/components/ui/TextRender.vue'
+
+const text = `wkmsadkw<a href="111">222</a>啊啊啊？`
+</script>
+
+<style lang="scss" scoped></style>
