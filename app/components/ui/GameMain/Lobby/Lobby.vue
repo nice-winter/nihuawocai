abc2235aa54d50729a0789ebb58d6b55ebfc81fb
nice-winter <doyouknowmeemail@gmail.com>
2025-11-05 22:33:23 +0800

    refactor(Lobby): 重构大厅页面，拆分房间列表至 sfc

--- app/components/ui/GameMain/Lobby/Lobby.vue
+++ app/components/ui/GameMain/Lobby/Lobby.vue
@@ -1,88 +1,13 @@
 <template>
   <div class="flex flex-row h-full">
-    <div class="flex flex-col w-[74.4%]">
-      <div class="h-[87.72%] flex flex-wrap relative">
-        <div class="z-1 flex flex-wrap">
-          <UiGameMainLobbyRoomListItem
-            v-for="room in Array.from(rooms).slice(0, 6)"
-            :key="room.roomNumber"
-            :room-info="room"
-            @join-button-click="join"
-            @look-button-click="look"
-          />
-        </div>
-        <div class="flex flex-wrap w-full h-full absolute">
-          <div
-            v-for="i in 6"
-            :key="i + Date.now()"
-            class="w-1/2 border border-white/60 custom-border-item"
-          />
-        </div>
-      </div>
-      <div
-        class="grow flex items-center gap-4 p-[0.785rem] bg-[#f1d0ae42] border-t-2 border-white/60"
-      >
-        <UiButton size="lg" color="red" @click="() => console.log('快速开始')"> 快速开始 </UiButton>
-        <div class="px-4 flex gap-2 items-center">
-          <UInput
-            v-model="roomNumberInputValue"
-            size="xs"
-            placeholder="房间号"
-            maxlength="3"
-            class="game-input w-14"
-            style="background-color: #fff"
-          />
-          <UiButton size="sm" :disabled="roomNumberInputValue === ''">加入</UiButton>
-        </div>
-        <UiButton>等待房间</UiButton>
-        <UiButton>创建房间</UiButton>
-        <div class="px-4 flex gap-8 items-center ml-auto">
-          <UiButton type="arrow-left" color="red" />
-          <UiButton type="arrow-right" color="red" />
-        </div>
-      </div>
-    </div>
+    <UiGameMainLobbyRoomList class="w-[74.4%]" />
+
     <div class="grow flex flex-col bg-[#f1d0ae42] border-l-2 border-white/60">
       <UiGameMainChatPanel class="grow max-w-[227.89px]" />
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { roomList } from '~/stores/test'
+<script setup lang="ts"></script>
 
-const roomNumberInputValue = ref('')
-const rooms = ref(roomList)
-
-const join = (roomNumber: number) => {
-  console.log(`[join]`, roomNumber)
-}
-
-const look = (roomNumber: number) => {
-  console.log(`[look]`, roomNumber)
-}
-</script>
-
-<style scoped>
-.custom-border-item:nth-child(1),
-.custom-border-item:nth-child(2) {
-  border-top: none;
-}
-
-.custom-border-item:nth-child(5),
-.custom-border-item:nth-child(6) {
-  border-bottom: none;
-}
-
-.custom-border-item:nth-child(1),
-.custom-border-item:nth-child(3),
-.custom-border-item:nth-child(5) {
-  border-left: none;
-}
-
-.custom-border-item:nth-child(2),
-.custom-border-item:nth-child(4),
-.custom-border-item:nth-child(6) {
-  border-right: none;
-}
-</style>
+<style scoped></style>
--- app/components/ui/GameMain/Lobby/RoomList.vue
+++ app/components/ui/GameMain/Lobby/RoomList.vue
@@ -1,7 +1,86 @@
 <template>
-  <div></div>
+  <div class="flex flex-col">
+    <div class="h-[87.72%] flex flex-wrap relative">
+      <div class="z-1 flex flex-wrap">
+        <UiGameMainLobbyRoomListItem
+          v-for="room in Array.from(rooms).slice(0, 6)"
+          :key="room.roomNumber"
+          :room-info="room"
+          @join-button-click="join"
+          @look-button-click="() => join(room.roomNumber, true)"
+        />
+      </div>
+      <div class="flex flex-wrap w-full h-full absolute">
+        <div
+          v-for="i in 6"
+          :key="i + Date.now()"
+          class="w-1/2 border border-white/60 custom-border-item"
+        />
+      </div>
+    </div>
+    <div
+      class="grow flex items-center gap-4 p-[0.785rem] bg-[#f1d0ae42] border-t-2 border-white/60"
+    >
+      <UiButton size="lg" color="red" @click="quickMatch"> 快速开始 </UiButton>
+      <div class="px-4 flex gap-2 items-center">
+        <UInput
+          v-model="roomNumberInputValue"
+          size="xs"
+          placeholder="房间号"
+          maxlength="3"
+          class="game-input w-14"
+          style="background-color: #fff"
+        />
+        <UiButton
+          size="sm"
+          :disabled="roomNumberInputValue === ''"
+          @click="() => join(Number(roomNumberInputValue))"
+          >加入</UiButton
+        >
+      </div>
+      <UiButton @click="showOnlyWaitingRooms = !showOnlyWaitingRooms">{{
+        showOnlyWaitingRooms ? '等待房间' : '全部房间'
+      }}</UiButton>
+      <UiButton>创建房间</UiButton>
+      <div class="px-4 flex gap-8 items-center ml-auto">
+        <UiButton type="arrow-left" color="red" @click="prevPage" />
+        <UiButton type="arrow-right" color="red" @click="nextPage" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { roomList } from '~/stores/test'
 
-<style scoped></style>
+const gameRoomListStore = useGameRoomListStore()
+const { join, prevPage, nextPage, quickMatch } = gameRoomListStore
+const { showOnlyWaitingRooms } = storeToRefs(gameRoomListStore)
+
+const roomNumberInputValue = ref('')
+const rooms = ref(roomList)
+</script>
+
+<style scoped>
+.custom-border-item:nth-child(1),
+.custom-border-item:nth-child(2) {
+  border-top: none;
+}
+
+.custom-border-item:nth-child(5),
+.custom-border-item:nth-child(6) {
+  border-bottom: none;
+}
+
+.custom-border-item:nth-child(1),
+.custom-border-item:nth-child(3),
+.custom-border-item:nth-child(5) {
+  border-left: none;
+}
+
+.custom-border-item:nth-child(2),
+.custom-border-item:nth-child(4),
+.custom-border-item:nth-child(6) {
+  border-right: none;
+}
+</style>
