4d7b938f6d757d86a3714e315f0054c9459da3eb
nice-winter <doyouknowmeemail@gmail.com>
2025-11-07 01:56:04 +0800

    refactor(ws): 重构 ws 主模块

--- server/ws/utils/index.ts
+++ server/ws/utils/index.ts
@@ -2,26 +2,28 @@ import type { WebsocketMessage } from '#shared/interfaces/ws'
 import type { UserData } from '#shared/interfaces/userData'
 import type { AdapterInternal, Peer, WSError } from 'crossws'
 
+type WsPeer = Peer<AdapterInternal>
+
 type WsEvents = {
   'ws:connect': {
     peer: Peer<AdapterInternal>
-    user: UserData
+    user: UserData | null
     reply: <T>(msg: WebsocketMessage<T>) => void
   }
   'ws:message': {
     peer: Peer<AdapterInternal>
     msg: WebsocketMessage
-    user: UserData
+    user: UserData | null
     reply: <T>(msg: WebsocketMessage<T>) => void
   }
   'ws:error': {
     peer: Peer<AdapterInternal>
-    user: UserData
+    user: UserData | null
     error: WSError
   }
   'ws:disconnect': {
     peer: Peer<AdapterInternal>
-    user: UserData
+    user: UserData | null
     code?: number
     reason?: string
   }
@@ -31,8 +33,24 @@ interface WsHandlers {
   [key: string]: (e: WsEvents['ws:message']) => void | Promise<void>
 }
 
+const isOpen = (peer: WsPeer) => peer.websocket && peer.websocket.readyState === 1
+
+const safeSend = (peer: WsPeer, encoded: ArrayBuffer | string) => {
+  if (isOpen(peer)) {
+    try {
+      peer.send(encoded)
+    } catch (e) {
+      console.warn('[ws]', 'safeSend failed', e)
+    }
+  }
+}
+
+const reply = (peer: WsPeer) => (msg: WebsocketMessage) => {
+  safeSend(peer, encode({ ...msg, _t: Date.now(), _reply: true }))
+}
+
 const defineWsHandlers = (handlers: WsHandlers) => {
   return handlers
 }
 
-export { WsEvents, WsHandlers, defineWsHandlers }
+export { type WsPeer, type WsEvents, type WsHandlers, isOpen, safeSend, reply, defineWsHandlers }
