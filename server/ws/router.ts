ec7403e05bab057d8d01519c82f714856b8243d7
nice-winter <doyouknowmeemail@gmail.com>
2025-11-08 05:54:47 +0800

    feat(ws-router): 注册的 Handlers 中发生错误时，自动响应错误信息

--- server/ws/router.ts
+++ server/ws/router.ts
@@ -2,6 +2,8 @@ import { wsEventBus } from '.'
 import type { WsHandlers } from './utils'
 import roomListHandler from './handlers/room-list'
 
+const log = (...data: unknown[]) => console.log('[ws-router]', ...data)
+
 function registerHandlers(handlers: WsHandlers) {
   wsEventBus.on('ws:message', async (e) => {
     const handler = handlers[e.msg.type]
@@ -9,16 +11,12 @@ function registerHandlers(handlers: WsHandlers) {
       try {
         await handler(e)
       } catch (err) {
-        // e.reply({ type: 'error', message: String(err) })
+        e.reply({ type: e.msg.type, message: String(err), _error: true })
       }
     }
   })
 
-  console.log(
-    '[ws-router]',
-    `注册了 ${Object.keys(handlers).length} 个 Handlers:`,
-    Object.keys(handlers).join(', ')
-  )
+  log(`注册了 ${Object.keys(handlers).length} 个 Handlers:`, Object.keys(handlers).join(', '))
 }
 
 export default function () {
