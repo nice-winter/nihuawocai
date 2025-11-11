7af21699d9bd5671286ac6dacf94001c8759100e
nice-winter <doyouknowmeemail@gmail.com>
2025-11-11 10:51:29 +0800

    feat(room*): 房间现在支持显示房间状态变化事件，例如：玩家进入退出等

--- app/composables/eventBus.ts
+++ app/composables/eventBus.ts
@@ -1,10 +1,35 @@
 import mitt from 'mitt'
+import type { Player } from '#shared/interfaces/player'
 
 type Events = {
   'user:logout': unknown
   'sketchpad:undo': unknown
   'sketchpad:redo': unknown
   'sketchpad:clear': unknown
+
+  // room
+  'current:room:event:player_join': {
+    seat: number
+    player: Player
+  }
+  'current:room:event:onlooker_join': {
+    player: Player
+  }
+  'current:room:event:player_leave': {
+    seat: number
+    player: Player
+  }
+  'current:room:event:onlooker_leave': {
+    player: Player
+  }
+  'current:room:event:onlooker_sit': {
+    seat: number
+    player: Player
+  }
+  'current:room:event:password_change': {
+    locked: boolean
+    password: string
+  }
 }
 
 export const eventBus = mitt<Events>()
