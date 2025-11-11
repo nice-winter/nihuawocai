9ce5ca87405fb431bb67db084040160774518761
nice-winter <doyouknowmeemail@gmail.com>
2025-11-11 12:17:52 +0800

    feat(MessageList): 消息列表支持右键清空和复制选中内容

--- app/components/ui/GameMain/MessageList.vue
+++ app/components/ui/GameMain/MessageList.vue
@@ -1,101 +1,104 @@
 <template>
-  <UiScrollBar
-    ref="MessageListScrollBar"
-    v-model:scroll-on-buttom="isScrollOnBottom"
-    :auto-hide="false"
-    :delay="1"
-    :style="{ '--scrollbar-color': '#bc966f', '--scrollbar-color-hover': '#cea57c' }"
-    :size="7"
-  >
-    <div class="message-list relative">
-      <div
-        v-show="autoScroll && showNewMessageIndicator && newMessageCount > 0"
-        class="absolute bottom-1 right-[0.8rem] flex flex-col"
-      >
+  <UContextMenu :items="contextMenuItems" size="sm" :press-open-delay="1">
+    <UiScrollBar
+      ref="MessageListScrollBar"
+      v-model:scroll-on-buttom="isScrollOnBottom"
+      :auto-hide="false"
+      :delay="1"
+      :style="{ '--scrollbar-color': '#bc966f', '--scrollbar-color-hover': '#cea57c' }"
+      :size="7"
+    >
+      <div class="message-list relative">
         <div
-          class="p-1 h-6 min-w-6 text-xs text-center text-white rounded-md select-none cursor-pointer"
-          style="background-color: rgb(75 54 32 / 57%); border-bottom-right-radius: unset"
-          @click="
-            () => {
-              scrollToBottom()
-              newMessageCount = 0
-            }
-          "
+          v-show="autoScroll && showNewMessageIndicator && newMessageCount > 0"
+          class="absolute bottom-1 right-[0.8rem] flex flex-col"
         >
-          <span>{{ newMessageCount }}</span>
-        </div>
-        <div>
-          <span
-            class="size-0 float-right"
-            style="border-top: 2px solid rgb(75 54 32 / 57%); border-left: 4px solid transparent"
-          />
-        </div>
-      </div>
-
-      <template v-for="(item, index) in messageList" :key="index + item.type">
-        <p
-          v-if="item.type === 'text'"
-          class="message-item"
-          :class="[`message-item-${item.type}`]"
-          :style="{
-            color: item.style?.color || 'inherit',
-            fontSize: item.style?.fontSize || `13px`,
-            fontWeight: item.style?.fontWeight || 'normal'
-          }"
-        >
-          {{ item.msg }}
-        </p>
-
-        <p v-else-if="item.type === 'chat'" class="py-[0.3rem] first:pt-0 last:pb-0">
-          <span class="text-[13px]">
-            <UiAvatar
-              class="size-6.5 align-top"
-              :player="item.sender"
-              :verified-icon="{ show: true, size: 12, right: -2, bottom: -2 }"
+          <div
+            class="p-1 h-6 min-w-6 text-xs text-center text-white rounded-md select-none cursor-pointer"
+            style="background-color: rgb(75 54 32 / 57%); border-bottom-right-radius: unset"
+            @click="
+              () => {
+                scrollToBottom()
+                newMessageCount = 0
+              }
+            "
+          >
+            <span>{{ newMessageCount }}</span>
+          </div>
+          <div>
+            <span
+              class="size-0 float-right"
+              style="border-top: 2px solid rgb(75 54 32 / 57%); border-left: 4px solid transparent"
             />
-            <span class="ml-2"> {{ item.sender.nickname }}： </span>
-            <span class="break-normal wrap-break-word">{{ item.msg }}</span>
-          </span>
-        </p>
+          </div>
+        </div>
 
-        <p v-else-if="item.type === 'action'">
-          <span class="text-[13px]">
-            <span class="text-pink-400">{{ item.sender.nickname }}</span>
-            {{ item.msg }}
-          </span>
-        </p>
-
-        <p v-else-if="item.type === 'system'">
-          <USeparator
-            class="select-none"
-            :label="item.msg"
-            :ui="{
-              border: `border-[#AE9783]`,
-              label: `text-[13px] text-[#AE9783]`
+        <template v-for="(item, index) in messageList" :key="index + item.type">
+          <p
+            v-if="item.type === 'text'"
+            class="message-item"
+            :class="[`message-item-${item.type}`]"
+            :style="{
+              color: item.style?.color || 'inherit',
+              fontSize: item.style?.fontSize || `13px`,
+              fontWeight: item.style?.fontWeight || 'normal'
             }"
-          />
-        </p>
+          >
+            {{ item.msg }}
+          </p>
 
-        <p v-else-if="item.type === 'broadcast'" class="py-[0.3rem] first:pt-0 last:pb-0">
-          <span class="text-[13px]">
-            <UiAvatar class="size-6.5 align-top" :player="item.sender" />
-            <span class="ml-2">{{ item.sender.nickname }}</span>
-            <span class="break-normal wrap-break-word">
-              在{{ item.roomNumber }}号房间喊道：赶快<UiLinkButton
-                color="red"
-                @click="joinFromBroadcast(item.roomNumber)"
-                >加入</UiLinkButton
-              >我们一起游戏吧！
+          <p v-else-if="item.type === 'chat'" class="py-[0.3rem] first:pt-0 last:pb-0">
+            <span class="text-[13px]">
+              <UiAvatar
+                class="size-6.5 align-top"
+                :player="item.sender"
+                :verified-icon="{ show: true, size: 12, right: -2, bottom: -2 }"
+              />
+              <span class="ml-2"> {{ item.sender.nickname }}： </span>
+              <span class="break-normal wrap-break-word">{{ item.msg }}</span>
             </span>
-          </span>
-        </p>
-      </template>
-    </div>
-  </UiScrollBar>
+          </p>
+
+          <p v-else-if="item.type === 'action'">
+            <span class="text-[13px]">
+              <span class="text-pink-400">{{ item.sender.nickname }}</span>
+              {{ item.msg }}
+            </span>
+          </p>
+
+          <p v-else-if="item.type === 'system'">
+            <USeparator
+              class="select-none"
+              :label="item.msg"
+              :ui="{
+                border: `border-[#AE9783]`,
+                label: `text-[13px] text-[#AE9783]`
+              }"
+            />
+          </p>
+
+          <p v-else-if="item.type === 'broadcast'" class="py-[0.3rem] first:pt-0 last:pb-0">
+            <span class="text-[13px]">
+              <UiAvatar class="size-6.5 align-top" :player="item.sender" />
+              <span class="ml-2">{{ item.sender.nickname }}</span>
+              <span class="break-normal wrap-break-word">
+                在{{ item.roomNumber }}号房间喊道：赶快<UiLinkButton
+                  color="red"
+                  @click="joinFromBroadcast(item.roomNumber)"
+                  >加入</UiLinkButton
+                >我们一起游戏吧！
+              </span>
+            </span>
+          </p>
+        </template>
+      </div>
+    </UiScrollBar>
+  </UContextMenu>
 </template>
 
 <script setup lang="ts">
 import type { Player } from '#shared/interfaces/player'
+import type { ContextMenuItem } from '@nuxt/ui'
 
 interface MessageListProps {
   autoScroll?: boolean
@@ -143,6 +146,22 @@ const { autoScroll = true, showNewMessageIndicator = false } = defineProps<Messa
 const messageList = reactive<IMessage[]>([])
 const newMessageCount = ref(0)
 const messageListScrollBarRef = useTemplateRef('MessageListScrollBar')
+const selectText = useTextSelection()
+const contextMenuItems = computed(() => {
+  const items: ContextMenuItem[] = [
+    {
+      label: '清空',
+      onSelect: () => clear()
+    },
+    {
+      label: '复制',
+      disabled: selectText.text.value == '',
+      onSelect: () => copy()
+    }
+  ]
+  return items
+})
+const { copy } = useClipboard({ source: selectText.text })
 
 const addMessage = (msg: IMessage) => {
   messageList.push(msg)
@@ -177,10 +196,13 @@ const joinFromBroadcast = (roomNumber: number) => {
   console.log(`[MessageList]`, `[JoinFromBroadcast]`, roomNumber)
 }
 
+const clear = () => messageList.splice(0, messageList.length)
+
 defineExpose({
   addMessage,
   scrollToBottom,
-  scrollToTop
+  scrollToTop,
+  clear
 })
 </script>
 
