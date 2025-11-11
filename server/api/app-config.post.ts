47ca31488db5839ff491b75df9d9bb189768e701
nice-winter <doyouknowmeemail@gmail.com>
2025-11-11 18:22:12 +0800

    style(): 注释

--- server/api/app-config.post.ts
+++ server/api/app-config.post.ts
@@ -2,10 +2,10 @@ import type { AppConfig } from '#shared/interfaces/appConfig'
 import { updateAppConfig } from '~~/server/services/app-config'
 
 export default defineEventHandler(async (event) => {
-  // @todo: 此处鉴权不仅需要登录，还需要验证其是否为管理员
+  // @TODO: 此处鉴权不仅需要登录，还需要验证其是否为管理员
   await requireUserSession(event)
 
   const appConfig = (await readBody(event)) as Partial<AppConfig>
-  // @todo: 参数验证
+  // @TODO: 参数验证
   return await updateAppConfig(appConfig)
 })
