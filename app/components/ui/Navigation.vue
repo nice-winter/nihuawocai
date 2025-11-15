4b6b5526f604f9047fd955a2c4a590319b5f12a2
nice-winter <doyouknowmeemail@gmail.com>
2025-11-15 15:01:19 +0800

    feat(Navigation): 导航图标现在使用雪碧图

--- app/components/ui/Navigation.vue
+++ app/components/ui/Navigation.vue
@@ -1,102 +1,84 @@
 <template>
-  <div class="h-6 flex gap-3 items-center">
-    <div class="size-6 nav-icon hi"></div>
+  <ul class="h-6 flex gap-3 items-center">
+    <li class="size-6 nav-icon hi"></li>
 
-    <div class="size-6 nav-icon package"></div>
+    <li class="size-6 nav-icon package"></li>
 
-    <div class="size-6 nav-icon camera"></div>
+    <li class="size-6 nav-icon camera"></li>
 
-    <div class="size-6 nav-icon sound-enabled"></div>
+    <li
+      class="size-6 nav-icon"
+      :class="sound ? 'sound-enabled' : 'sound-disabled'"
+      @click="sound = !sound"
+    ></li>
 
-    <div class="size-6 nav-icon help"></div>
+    <li class="size-6 nav-icon help"></li>
 
-    <div class="size-6 nav-icon github"></div>
-  </div>
+    <li class="size-6 nav-icon github"></li>
+  </ul>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const sound = ref(true)
+</script>
 
 <style scoped>
 .nav-icon {
   filter: drop-shadow(0px 0px 2px rgb(161, 87, 17));
   cursor: pointer;
+  background-image: url('~/assets/icons/navicon-sprites.png');
+  background-size: 100% auto;
+  background-repeat: no-repeat;
+  background-color: transparent;
+  --sprites-index: 0;
+  background-position: 0 calc(var(--sprites-index) * -24px);
 }
 
 .nav-icon.hi {
-  background:
-    url('~/assets/icons/button-hi.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 0;
 }
 .nav-icon.hi:hover {
-  background:
-    url('~/assets/icons/button-hi-hover.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 1;
 }
 
 .nav-icon.package {
-  background:
-    url('~/assets/icons/button-package.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 2;
 }
 .nav-icon.package:hover {
-  background:
-    url('~/assets/icons/button-package-hover.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 3;
 }
 
 .nav-icon.camera {
-  background:
-    url('~/assets/icons/button-camera.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 4;
 }
 .nav-icon.camera:hover {
-  background:
-    url('~/assets/icons/button-camera-hover.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 5;
 }
 
 .nav-icon.sound-enabled {
-  background:
-    url('~/assets/icons/button-sound-enabled.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 6;
 }
 .nav-icon.sound-enabled:hover {
-  background:
-    url('~/assets/icons/button-sound-enabled-hover.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 7;
+}
+.nav-icon.sound-disabled {
+  --sprites-index: 8;
+}
+.nav-icon.sound-disabled:hover {
+  --sprites-index: 9;
 }
 
 .nav-icon.help {
-  background:
-    url('~/assets/icons/button-help.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 10;
 }
 .nav-icon.help:hover {
-  background:
-    url('~/assets/icons/button-help-hover.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 11;
 }
 
 .nav-icon.github {
-  background:
-    url('~/assets/icons/button-github.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 12;
 }
 .nav-icon.github:hover {
-  background:
-    url('~/assets/icons/button-github-hover.png') no-repeat center center,
-    transparent;
-  background-size: contain;
+  --sprites-index: 13;
 }
 </style>
