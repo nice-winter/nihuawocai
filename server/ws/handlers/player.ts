bd1b88d2a4b27737e257647a2830d94f496664bd
nice-winter <doyouknowmeemail@gmail.com>
2025-11-16 14:53:28 +0800

    feat(Avatar): 现在获取玩家资料是实时性的

--- server/ws/handlers/player.ts
+++ server/ws/handlers/player.ts
@@ -1,12 +1,22 @@
 import { consola } from 'consola'
 import { defineWsHandlers } from '~~/server/ws/utils'
 import { say } from '~~/server/services/chat'
-import type { WebsocketMessage } from '#shared/interfaces/ws'
 import { getLobbyPlayers } from '~~/server/services/player'
+import { getUserData } from '~~/server/services/user'
+import type { WebsocketMessage } from '#shared/interfaces/ws'
 
 const logger = consola.withTag('Player Handler')
 
 export default defineWsHandlers({
+  'player:get_profile': async ({ msg, user }) => {
+    // @TODO: 这里需要验证传入参数
+    const { id } = msg as WebsocketMessage<{ id: string }>
+
+    return {
+      id,
+      profile: await getUserData(id)
+    }
+  },
   'player:lobby_players_pull': async ({ msg, user }) => {
     return {
       lobby_players: getLobbyPlayers()
