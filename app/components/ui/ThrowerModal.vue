df33e34825dcf53db89ba9f314f643f38bbbc791
nice-winter <doyouknowmeemail@gmail.com>
2025-11-19 20:02:53 +0800

    feat(ThrowerModal): 添加小回合结束时的抛道具 Modal

---
+++ app/components/ui/ThrowerModal.vue
@@ -0,0 +1,123 @@
+<template>
+  <Teleport :to="targetEl">
+    <div
+      v-if="visible"
+      class="z-114514 flex items-center justify-center bg-transparent"
+      :class="parent ? 'absolute inset-0' : 'fixed inset-0'"
+    >
+      <div ref="modalRef" class="bg-texture rounded-md w-90 shadow-hard flex flex-col" tabindex="0">
+        <div class="h-40 p-4">
+          <div class="flex flex-col gap-4 text-[#804B19] text-light select-none">
+            <div class="flex justify-between text-[13px]">
+              <span>所有人猜对</span>
+              <span
+                >倒计时：<span class="text-(--game-red)">{{ t }}</span></span
+              >
+            </div>
+
+            <p class="text-[13px] text-center">
+              答案：<span class="text-lg text-(--game-red) font-bold">【{{ answer }}】</span>
+            </p>
+
+            <div v-if="showThrowItem && !isThrowed" class="flex items-center justify-between px-20">
+              <div
+                class="size-9 flex items-center justify-center rounded-full bg-white cursor-pointer shadow-hard"
+                @click="() => throwItemClick('slipper')"
+              >
+                <UIcon name="custom:icon-slipper" class="size-7" />
+              </div>
+
+              <div
+                class="size-12 flex items-center justify-center rounded-full bg-white cursor-pointer shadow-hard"
+                @click="() => throwItemClick('flower')"
+              >
+                <UIcon name="custom:icon-flower" class="size-9" />
+              </div>
+
+              <div
+                class="size-9 flex items-center justify-center rounded-full bg-white cursor-pointer shadow-hard"
+                @click="() => throwItemClick('slipper')"
+              >
+                <UIcon name="custom:icon-slipper" class="size-7" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-center gap-8 p-4 bg-[#c5630e69]">
+          <UiButton color="red" @click="shared">分享美图</UiButton>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import type { Player } from '~~/shared/interfaces/player'
+
+export interface ThrowerModalProps {
+  answer?: ''
+  countdown?: number
+  bingoPlayers?: Player[]
+  missPlayers?: Player[]
+  showThrowItem?: boolean
+  parent?: Element
+}
+const {
+  parent = '',
+  answer = '',
+  countdown = 5,
+  bingoPlayers = [],
+  missPlayers = [],
+  showThrowItem = true
+} = defineProps<ThrowerModalProps>()
+
+const visible = ref(false)
+const resolveFn = ref<(() => void) | null>(null)
+const rejectFn = ref<(() => void) | null>(null)
+const modalRef = ref<HTMLDivElement | null>(null)
+const targetEl = computed(() => parent ?? document.body)
+
+const t = ref(countdown)
+const timer = ref<number | null>(null)
+const isThrowed = ref(false)
+
+const throwItemClick = (type: 'egg' | 'flower' | 'slipper') => {
+  console.log(type)
+  isThrowed.value = true
+}
+
+const open = (): Promise<void> => {
+  visible.value = true
+
+  return new Promise<void>((resolve, reject) => {
+    resolveFn.value = resolve
+    rejectFn.value = reject
+  })
+}
+
+const close = () => {
+  resolveFn.value?.()
+  visible.value = false
+  clearTimeout(timer.value!)
+}
+
+const shared = () => {
+  // 分享
+}
+
+onMounted(() => {
+  timer.value = setInterval(() => {
+    t.value--
+    if (t.value === 0) {
+      close()
+    }
+  }, 1000)
+})
+onUnmounted(() => {
+  clearTimeout(timer.value!)
+})
+
+defineExpose({ open })
+</script>
+
+<style scoped></style>
