15920bf9128fb6a2c04c897cf8938e1e53c4ed59
nice-winter <doyouknowmeemail@gmail.com>
2025-11-24 16:57:35 +0800

    feat(sketchpad): 画板根据 game store 的事件同步绘画、笔触切换等

--- app/stores/sketchpad/sketchpadStore.ts
+++ app/stores/sketchpad/sketchpadStore.ts
@@ -1,7 +1,12 @@
 import { eventBus } from '~/composables/eventBus'
 import type { Brush, BrushesNameString } from '~/interfaces/brush'
+import type { CachedPoint } from '~/components/ui/GameMain/Room/Playing/Sketchpad/fabric/SketchpadCanvas'
+import type { WebsocketMessage } from '~~/shared/interfaces/ws'
 
 export const useSketchpadStore = defineStore('sketchpad', () => {
+  const { wsEventBus, send } = useWsStore()
+  const gameStore = useGameStore()
+
   const options = reactive({
     colors: [
       '#000000',
@@ -50,42 +55,142 @@ export const useSketchpadStore = defineStore('sketchpad', () => {
     width: 1
   })
 
-  const setCurrentBrush = (brushName: BrushesNameString) => {
+  const currentBrushObjetc = computed(() =>
+    brushes.value.find((b) => b.name === currentBrush.value)
+  )
+
+  const setCurrentBrush = async (brushName: BrushesNameString) => {
     if (brushes.value.find((b) => b.name === brushName)) {
       currentBrush.value = brushName
+
+      await send({
+        type: 'game:drawing:sketchpad',
+        command: 'pencil_switch',
+        payload: {
+          name: brushName
+        }
+      })
     }
   }
 
-  const getCurrentBrushObjetc = () => {
-    return brushes.value.find((b) => b.name === currentBrush.value)
+  const updateBrushOptions = async (bo: Partial<typeof brushOptions>) => {
+    Object.assign(brushOptions, bo)
+
+    await send({
+      type: 'game:drawing:sketchpad',
+      command: 'pencil_options_update',
+      payload: {
+        options: brushOptions
+      }
+    })
   }
 
-  const updateBrushOptions = (bo: Partial<typeof brushOptions>) => {
-    Object.assign(brushOptions, bo)
-    console.log(`[Sketchpad-Store]`, brushOptions)
+  const draw = async (points: CachedPoint[]) => {
+    await send({
+      type: 'game:drawing:sketchpad',
+      command: 'draw',
+      payload: {
+        points: points.map((p) => {
+          return {
+            ...p,
+            point: {
+              x: p.point.x,
+              y: p.point.y
+            }
+          }
+        })
+      }
+    })
   }
 
   const undo = () => {
     eventBus.emit('sketchpad:undo')
-    console.log('[Sketchpad-Store]', 'undo')
   }
   const redo = () => {
     eventBus.emit('sketchpad:redo')
-    console.log('[Sketchpad-Store]', 'redo')
   }
-  const clear = () => {
+  const clear = async () => {
     eventBus.emit('sketchpad:clear')
-    console.log('[Sketchpad-Store]', 'clear')
+
+    await send({
+      type: 'game:drawing:sketchpad',
+      command: 'clear'
+    })
   }
+
+  const handleSketchpadEvents = (
+    command: 'pencil_switch' | 'pencil_options_update' | 'draw' | 'clear',
+    payload: unknown
+  ) => {
+    switch (command) {
+      // 切换笔触
+      case 'pencil_switch': {
+        // @TODO: 笔触名称存在验证
+        const { name } = payload as { name: BrushesNameString }
+        currentBrush.value = name
+        break
+      }
+      // 更新笔触设置
+      case 'pencil_options_update': {
+        const { options } = payload as { options: { color: string; width: number } }
+        Object.assign(brushOptions, options)
+        break
+      }
+      // 绘画
+      case 'draw': {
+        const { points } = payload as {
+          points: {
+            point: {
+              x: number
+              y: number
+            }
+            sequence: number
+            timestamp: number
+            action: 'down' | 'move' | 'up'
+          }[]
+        }
+        eventBus.emit('sketchpad:draw', {
+          points
+        })
+        break
+      }
+      // 清空画板
+      case 'clear': {
+        eventBus.emit('sketchpad:clear')
+        break
+      }
+    }
+  }
+
+  wsEventBus.on('ws:message', (msg) => {
+    if (msg.type.startsWith('game:event:sketchpad')) {
+      const { command, payload } = msg as WebsocketMessage<{
+        command: 'pencil_switch' | 'pencil_options_update' | 'draw' | 'clear'
+        payload: unknown
+      }>
+      handleSketchpadEvents(command, payload)
+    }
+  })
+
+  useEventBus('game:event:drawing:start', () => {
+    if (gameStore.isMyTurn) {
+      setCurrentBrush('pencil')
+      updateBrushOptions({
+        color: '#000000',
+        width: 1
+      })
+    }
+  })
 
   return {
     options,
     brushes,
     brushOptions,
     currentBrush,
+    currentBrushObjetc,
     setCurrentBrush,
-    getCurrentBrushObjetc,
     updateBrushOptions,
+    draw,
     undo,
     redo,
     clear
