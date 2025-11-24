e905960dbd8987c258320e5d68aaa41b4286832d
nice-winter <doyouknowmeemail@gmail.com>
2025-11-24 16:54:40 +0800

    feat(game): 添加游戏逻辑主模块

---
+++ server/ws/handlers/game.ts
@@ -0,0 +1,27 @@
+import { consola } from 'consola'
+import { defineWsHandlers } from '~~/server/ws/utils'
+import { handleGiveUp, handleGift, handleSketchpad } from '~~/server/services/game'
+import type { WebsocketMessage } from '#shared/interfaces/ws'
+
+const logger = consola.withTag('Game Handler')
+
+export default defineWsHandlers({
+  'game:drawing:give_up': async ({ user }) => {
+    return handleGiveUp(user.id)
+  },
+  'game:drawing:sketchpad': async ({ msg, user }) => {
+    const { command, payload } = msg as WebsocketMessage<{
+      command: 'pencil_switch' | 'pencil_options_update' | 'draw' | 'clear'
+      payload: unknown
+    }>
+    return await handleSketchpad(user.id, command, payload)
+  },
+  'game:interaction:gift': async ({ msg, user }) => {
+    const { item_type, count } = msg as WebsocketMessage<{
+      item_type: 'flower' | 'egg' | 'slipper'
+      count: number
+    }>
+
+    return handleGift(user.id, item_type)
+  }
+})
