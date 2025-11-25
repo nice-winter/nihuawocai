e099f3f5916bb76d3d9dd1c51d2898434ed646b7
nice-winter <doyouknowmeemail@gmail.com>
2025-11-26 05:47:04 +0800

    fix(room): 修复 UI 最大旁观人数显示错误的问题

--- shared/types/room.ts
+++ shared/types/room.ts
@@ -1,29 +1,44 @@
 import type { AppConfig } from './appConfig'
 import type { Player } from './player'
 
-type Players = (Player | null)[]
+interface RoomOptions {
+  /** 房间密码 */
+  password: string | ''
+  /** 最大旁观人数 */
+  maxOnlookers: number
+  /** 词库合集 ID 列表 */
+  libIds: string[]
+}
 
+type RoomConfig = AppConfig['game']['room']
+
+type RoomPlayers = (Player | null)[]
+
+/**
+ * 房间信息（通常为非房间内玩家展示用，例如：大厅房间列表展示）
+ * 有别于 Room，其内部的一些属性出于安全性可能会被替换或移除
+ */
 interface RoomInfo {
+  id: string
+  config: Partial<RoomConfig> | null
+  options: RoomOptions
+
   roomNumber: number
   owner: string
-  players: Players
+  players: RoomPlayers
   onlookers: Player[]
   seats: boolean[]
   playing: boolean
   locked: boolean
 }
 
-interface RoomOptions {
-  password: string | ''
-  maxOnlookers: number
-}
-
-type RoomConfig = AppConfig['game']['room']
-
+/**
+ * 完整的房间对象
+ */
 interface Room extends RoomInfo {
   id: string
   options: RoomOptions
-  config: RoomConfig | null
+  config: Partial<RoomConfig> | null
 }
 
 export type { RoomInfo, RoomOptions, RoomConfig, Room }
