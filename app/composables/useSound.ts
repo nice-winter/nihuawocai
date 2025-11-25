ff15e02ad991ce79455b455d7e189820dbee8858
nice-winter <doyouknowmeemail@gmail.com>
2025-11-26 02:24:28 +0800

    feat(sounds): 添加结束和放弃音效

--- app/composables/useSound.ts
+++ app/composables/useSound.ts
@@ -4,7 +4,9 @@ export const useSound = () => {
   const baseUrl = `${window.location.origin}/_nuxt/assets/sounds`
   const audioMap = {
     clock: new Audio(`${baseUrl}/clock.mp3`),
-    pop: new Audio(`${baseUrl}/pop.mp3`)
+    pop: new Audio(`${baseUrl}/pop.mp3`),
+    giveUp: new Audio(`${baseUrl}/give-up.mp3`),
+    end: new Audio(`${baseUrl}/end.mp3`)
   }
 
   const playSound = (name: keyof typeof audioMap) => {
