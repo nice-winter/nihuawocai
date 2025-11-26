6c2f89001f2964b382dab50a2b042840c008d891
nice-winter <doyouknowmeemail@gmail.com>
2025-11-26 16:12:47 +0800

    feat(sound): 添加答对音效

--- app/composables/useSound.ts
+++ app/composables/useSound.ts
@@ -1,24 +1,53 @@
+import { Howl } from 'howler'
+import { useSettingStore } from '@/stores/setting'
+
 export const useSound = () => {
   const settingStore = useSettingStore()
 
   const baseUrl = `${window.location.origin}/_nuxt/assets/sounds`
-  const audioMap = {
-    clock: new Audio(`${baseUrl}/clock.mp3`),
-    pop: new Audio(`${baseUrl}/pop.mp3`),
-    giveUp: new Audio(`${baseUrl}/give-up.mp3`),
-    end: new Audio(`${baseUrl}/end.mp3`)
+
+  const soundMap = {
+    clock: new Howl({
+      src: [`${baseUrl}/clock.mp3`],
+      preload: true,
+      loop: true
+    }),
+    pop: new Howl({
+      src: [`${baseUrl}/pop.mp3`],
+      preload: true
+    }),
+    bingo: new Howl({
+      src: [`${baseUrl}/bingo.mp3`],
+      volume: 0.6,
+      preload: true
+    }),
+    giveUp: new Howl({
+      src: [`${baseUrl}/give-up.mp3`],
+      preload: true
+    }),
+    end: new Howl({
+      src: [`${baseUrl}/end.mp3`],
+      preload: true
+    })
   }
 
-  const playSound = (name: keyof typeof audioMap) => {
+  // 播放
+  const playSound = (name: keyof typeof soundMap) => {
     if (!settingStore.settings.sound) return
+    const sound = soundMap[name]
+    if (!sound) return
+    sound.play()
+  }
 
-    const audio = audioMap[name]
-    if (!audio) return
-    audio.currentTime = 0
-    audio.play()
+  // 停止某个音效
+  const stopSound = (name: keyof typeof soundMap) => {
+    const sound = soundMap[name]
+    if (!sound) return
+    sound.stop()
   }
 
   return {
-    playSound
+    playSound,
+    stopSound
   }
 }
