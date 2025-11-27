168f394e5d57f9d0b7c2295aba570bdf21bc6673
nice-winter <doyouknowmeemail@gmail.com>
2025-11-28 05:09:35 +0800

    perf(crypto): 优化编码方式

--- shared/utils/crypto.ts
+++ shared/utils/crypto.ts
@@ -1,18 +1,21 @@
-import * as cbor from 'cbor2'
+import { encode as _encode, decode as _decode } from 'cbor2'
 
 /**
  * 将对象编码为 ArrayBuffer
  */
 const encode = <T = unknown>(data: T): ArrayBuffer => {
-  const encoded = cbor.encode(data)
-  return encoded.buffer as ArrayBuffer
+  const encoded = _encode(data)
+  return encoded.buffer.slice(
+    encoded.byteOffset,
+    encoded.byteOffset + encoded.byteLength
+  ) as ArrayBuffer
 }
 
 /**
  * 将 ArrayBuffer 解码为对象
  */
 const decode = <T = unknown>(buffer: Uint8Array<ArrayBufferLike>): T => {
-  const decoded = cbor.decode(buffer) as T
+  const decoded = _decode(buffer) as T
   return decoded
 }
 
