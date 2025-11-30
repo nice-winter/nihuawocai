2e21bb253ef87598aea3606abfec95df6f7579a2
nice-winter <doyouknowmeemail@gmail.com>
2025-11-30 23:50:32 +0800

    feat(setting): 添加玩家设置持久化功能

--- app/stores/setting.ts
+++ app/stores/setting.ts
@@ -1,7 +1,39 @@
+import { useStorage } from '@vueuse/core'
+
 export const useSettingStore = defineStore('setting', () => {
+  const playerStore = usePlayerStore()
+  const userId = computed(() => playerStore.loggedInPlayer?.id ?? 'guest')
+
   const settings = reactive({
     sound: true
   })
+
+  const storageKey = computed(() => `settings:${userId.value}`)
+
+  const loadFromStorage = () => {
+    const stored = useStorage(storageKey.value, {
+      sound: true
+    })
+
+    Object.assign(settings, stored.value)
+  }
+
+  watch(
+    userId,
+    () => {
+      loadFromStorage()
+    },
+    { immediate: true }
+  )
+
+  watch(
+    settings,
+    (val) => {
+      const stored = useStorage(storageKey.value, val)
+      stored.value = val
+    },
+    { deep: true }
+  )
 
   const switchSound = () => {
     settings.sound = !settings.sound
